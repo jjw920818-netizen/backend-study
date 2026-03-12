a99ca236ec66e97ca936739591d27d2cff0e0a52
jjw920818-netizen <jjw920818@gmail.com>
2026-03-12 13:19:41 +0900

    폴더정리

--- project1/response.py
+++ project1/response.py
@@ -19,3 +19,11 @@ class HealthProfileResponse(BaseModel):
     weight_kg: float
     smoking: bool
     exercise_per_week: int
+
+class HealthRiskPredictionResponse(BaseModel):
+    id: int
+    user_id: int
+    diabates_probability: float
+    hypertension_probability: float
+    # model_version -> 클라이언트에게 공개하지 않을 수 있음
+    created_at: datetime
--- project1/routers/prediction.py
+++ project1/routers/prediction.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter, Depends, status, HTTPException
 
 from auth.jwt import verify_user
 from database.connection import get_session
-from database.orm import HealthProfile
+from database.orm import HealthProfile, HealthRiskPrediction
+from llm import predict_health_risk
+from response import HealthRiskPredictionResponse
 
 router = APIRouter(tags=["Prediction"])
 
@@ -11,6 +13,7 @@ router = APIRouter(tags=["Prediction"])
     "/predictions",
     summary="당뇨병/고혈압 위험도 예측 API",
     status_code=status.HTTP_201_CREATED,
+    response_model=HealthRiskPredictionResponse
 )
 async def risk_predict_handler(
     user_id: int = Depends(verify_user),
@@ -27,6 +30,19 @@ async def risk_predict_handler(
 
 
     # [2] profile로 위험도 예측 -> Open API
+    model_version = "gpt-5mini"
+    risk_prediction = await predict_health_risk(profile=profile, model_version=model_version)
+
     # [3] 결과(prediction) 저장
+    new_prediction = HealthRiskPrediction(
+        uwer_id=user_id,
+        diabetes_probability=risk_prediction.diabates_probability,
+        hypertension_probabilty=risk_prediction.hypertension_probability,
+        model_version= model_version,
+    )
+    session.add(new_prediction)
+    await session.commit()
+    await session.refrsh(new_prediction)
+
     return 
 
